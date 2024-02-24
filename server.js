bc88b4f9d387bc8bc4f0edb562cad5415fb11bb9
GustavoDeSouzaFonseca <rm94067@fiap.com.br>
2024-02-24 01:35:45 -0300

    build: :construction_worker: Remove workflow to new deploy

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ import app from './src/app.js';
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => console.log(`App listening at ${PORT}`));
+app.listen(PORT, () => console.log('App listening'));
