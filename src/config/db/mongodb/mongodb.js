6196c35eab70079f3a4cb2b7c6560a9c1a1392f0
GustavoDeSouzaFonseca <rm94067@fiap.com.br>
2024-02-25 15:09:12 -0300

    refactor: :recycle: Refactor mongodb connection

--- src/config/db/mongodb/mongodb.js
+++ src/config/db/mongodb/mongodb.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-mongoose.connect('mongodb+srv://dbagm:Gear2024@cl-gm-mongodb-dev.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000');
+mongoose.connect('mongodb+srv://dbagm:Gear2024@cl-gear-meeting-mongodb-prod.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000');
 
 const db = mongoose.connection;
 
